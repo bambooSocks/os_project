54ba7bcc3d2029276373929bdbb8177ab768f4ea
Matej <majtan.matej@gmail.com>
2020-09-27 18:06:16 +0200

    finished db3

--- db3/main.c
+++ db3/main.c
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include "linkedlist.h"
 
-
-
 int main() {
-    
+    int counter = 0;
+    linked_list_t *ll = newLL();
+    int isRunning = 1;
+
+    while (isRunning) {
+        char ch = fgetc(stdin);
+        switch (ch) {
+        case 'a':
+            pushToLL(ll, counter);
+            counter++;
+            break;
+        case 'b':
+            counter++;
+            break;
+        case 'c':
+            popFromLL(ll, NULL);
+            counter++;
+            break;
+        default:
+            printLL(ll);
+            isRunning = 0;
+        }
+    }
 
     return 0;
 }
